dd91579450007b6df33f5ffdac8fe40606742216
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-04-12 22:52:53 -0400

    Animate cars

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const time = document.querySelector('#time');
     const score = document.querySelector('#score');
     const startBtn = document.querySelector('#start');
+    const carLeft = document.querySelector('.car-left')
+    const carRight = document.querySelector('.car-right')
+    const logLeft = document.querySelector('.log-left')
+    const logRight = document.querySelector('.log-right')
     const width = 9;
     let currentIndex = 76;
     let timer;
@@ -30,6 +34,43 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[currentIndex].classList.add('froggy');
         fail()
         win()
+    }
+    //Move cars
+    function moveCars() {
+        carLeft.forEach(carLeft => moveCarLeft(carLeft));
+        carRight.forEach(carRight => moveCarRight(carRight));
+    }
+    //Move car left
+    function moveCarLeft(carLeft) {
+        switch (true) {
+            case carLeft.classList.contains('c1'):
+                carLeft.classList.remove('c1');
+                carLeft.classList.add('c2');
+                break
+            case carLeft.classList.contains('c2'):
+                carLeft.classList.remove('c2');
+                carLeft.classList.add('c3');
+                break
+            case carLeft.classList.contains('c3'):
+                carLeft.classList.remove('c3');
+                carLeft.classList.add('c1');
+                break
+        }
+    }
+    //Move car right
+    switch (true) {
+        case carRight.classList.contains('c1'):
+            carRight.classList.remove('c1');
+            carRight.classList.add('c3');
+            break
+        case carRight.classList.contains('c2'):
+            carRight.classList.remove('c2');
+            carRight.classList.add('c2');
+            break
+        case carRight.classList.contains('c3'):
+            carRight.classList.remove('c3');
+            carRight.classList.add('c2');
+            break
     }
 
 })
