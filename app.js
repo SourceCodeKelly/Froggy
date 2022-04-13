ca5aaf2ef42275c8251c7c8640cbed37d7f69979
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-04-12 23:06:22 -0400

    Animate logs

--- app.js
+++ app.js
@@ -58,19 +58,78 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     //Move car right
-    switch (true) {
-        case carRight.classList.contains('c1'):
-            carRight.classList.remove('c1');
-            carRight.classList.add('c3');
-            break
-        case carRight.classList.contains('c2'):
-            carRight.classList.remove('c2');
-            carRight.classList.add('c2');
-            break
-        case carRight.classList.contains('c3'):
-            carRight.classList.remove('c3');
-            carRight.classList.add('c2');
-            break
+    function moveCarRight() {
+        switch (true) {
+            case carRight.classList.contains('c1'):
+                carRight.classList.remove('c1');
+                carRight.classList.add('c3');
+                break
+            case carRight.classList.contains('c2'):
+                carRight.classList.remove('c2');
+                carRight.classList.add('c2');
+                break
+            case carRight.classList.contains('c3'):
+                carRight.classList.remove('c3');
+                carRight.classList.add('c2');
+                break
+        }
+    }
+
+    //Move logs
+    function moveLogs() {
+        logLeft.forEach(logLeft => moveLogLeft(logLeft));
+        logRight.forEach(logRight => moveLogRight(logRight));
+    }
+
+    //Move logs left
+    function moveLogLeft(logLeft) {
+        switch (true) {
+            case logLeft.classList.contains('l1'):
+                logLeft.classList.remove('l1');
+                logLeft.classList.add('l2');
+                break
+            case logLeft.classList.contains('l2'):
+                logLeft.classList.remove('l2');
+                logLeft.classList.add('l3');
+                break
+            case logLeft.classList.contains('l3'):
+                logLeft.classList.remove('l3');
+                logLeft.classList.add('l4');
+                break
+            case logLeft.classList.contains('l4'):
+                logLeft.classList.remove('l4');
+                logLeft.classList.add('l5');
+                break
+            case logLeft.classList.contains('l5'):
+                logLeft.classList.remove('l5');
+                logLeft.classList.add('l1');
+                break
+        }
+    }
+    //Move car right
+    function moveLogRight(logRight) {
+        switch (true) {
+            case logRight.classList.contains('l1'):
+                logRight.classList.remove('l1');
+                logRight.classList.add('l5');
+                break
+            case logRight.classList.contains('l2'):
+                logRight.classList.remove('l2');
+                logRight.classList.add('l1');
+                break
+            case logRight.classList.contains('l3'):
+                logRight.classList.remove('l3');
+                logRight.classList.add('l2');
+                break
+            case logRight.classList.contains('l4'):
+                logRight.classList.remove('l4');
+                logRight.classList.add('l3');
+                break
+            case logRight.classList.contains('l5'):
+                logRight.classList.remove('l5');
+                logRight.classList.add('l4');
+                break
+        }
     }
 
 })
